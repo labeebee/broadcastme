76138f09f77c000ed3c02079b71f19246c97e24f
abu <abukilayil@gmail.com>
2016-09-06 14:36:33 +0530

    RED try to test the method which for get the details of account

--- test_me.py
+++ test_me.py
@@ -1,12 +1,22 @@
-from broadcast_me import load_file,add_channel,tweet
-from twitter import tweet
+
+from twitter import load_data,details
+import twitter
 
 
 
-def test_tweet():
-    b=tweet("one")
-    assert len(b.ac)>0
+def test_load_data():
+    b=load_data("one")
+    assert len(twitter.acc)>0
 
+
+
+def test_details():
+    a,b,c,d=details("one")
+    assert a=="consumer_key"
+    assert b=="consumer_secret"
+    assert c=="access_token"
+    assert d=="access_secret"
+    
 
     
     

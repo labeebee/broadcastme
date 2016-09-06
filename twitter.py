dac518ed9cc2c89c2f364624f563bb07408fa931
abu <abukilayil@gmail.com>
2016-09-06 14:43:51 +0530

    GREEN pass the test by assign the account details into variables and return it

--- twitter.py
+++ twitter.py
@@ -8,4 +8,10 @@ def load_data(id):
     
 def details(id):
     ac=load_data(id)
+    consumer_key=ac[0]
+    consumer_secret=ac[1]
+    access_token=ac[2]
+    access_secret=ac[3]
+    return(consumer_key,consumer_secret,access_token,access_secret)
+    
     

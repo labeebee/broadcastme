6d1b30d4ce1ddea6ba5733329aa32feffb5aea8d
abu <abukilayil@gmail.com>
2016-09-06 22:20:32 +0530

    Pass the test by adding a new method new_twitter which add details into twitter_db

--- tweety.py
+++ tweety.py
@@ -1,12 +1,16 @@
 from broadcast_me import load_file
 from tweepy import OAuthHandler
 import tweepy
+import json
+
+
 def load_data(id):
     data=load_file("twitter_db.json")
     global acc
     acc=data[id]
     return (acc)
-    
+
+
 def details(id):
     ac=load_data(id)
     consumer_key=ac[0]
@@ -22,6 +26,20 @@ def posting (id,msg):
     auth.set_access_token(atoken,asec)
     tweepy.API(auth).update_status(msg)
     print("tweet successfully")
+
+
+    
+def new_twitter(uid,ck,cs,at,asc):
+    with open ("twitter_db.json") as f:
+        data=json.load(f)
+        if uid not in data:
+            data[uid]=[ck,cs,at,asc]
+            f=open("twitter_db.json","w")
+            json.dump(data,f)
+            f.close()
+        else:
+            print("User id Already Exist")
+        return (data)
     
     
     

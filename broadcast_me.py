6df7e05061b735b877abf69a1860efa544ae3b5d
saheed cp <saheed887@gmail.com>
2016-09-06 01:52:47 -0700

    GREEN implemented successfullt

--- broadcast_me.py
+++ broadcast_me.py
@@ -13,3 +13,15 @@ def add_channel(title,u_name,pwd,key):
         f=open("data.json","w")
         json.dump(data,f)
         f.close()
+
+def del_channel(key):
+	data = load_file("data.json")
+	if key in data:
+		del data[key]
+		print 'removed data'
+		open("data.json", "w").write(json.dumps(data,sort_keys=True,indent=4,separators=(',', ':')))
+	else:
+		print 'Unable to find the key'
+	return data
+if __name__ == '__main__':
+	del_channel('fb')

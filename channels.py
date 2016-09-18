8550d532b2ef79a28cbe5f5044d1d8db4858bce3
Labeeb <labeebg2@gmail.com>
2016-09-18 12:16:50 +0530

    Implements Unit test and Dependency injection

--- channels.py
+++ channels.py
@@ -4,28 +4,20 @@ import requests
 import fb_data
 
 
-data = fb_data.load_file("fb_data.json")
  
 
 class FaceBook:
-
      
-     
-     def __init__(self, username, password):
-         self.username = username
-         self.password = password
+     def __init__(self, username, password, filename):
+          self.username = username
+          self.password = password
+          self.filename = filename
 
-     def authenticate(self, username, password):
-         # Currently we are passing the Access Token manually
-         # via the variable above. However in the future we will be using
-         # this method.
-         return access_token
 
      def facebook_post(self, message):
-         access_token = data[self.username][2]
-         graph = facebook.GraphAPI(access_token)
-         profile = graph.get_object(data[self.username][1])
-         if (graph.put_wall_post(message)):
-             acknw = True
-             return acknw
-         
+          data = fb_data.load_file(self.filename)
+          access_token = data[self.username][2]
+          graph = facebook.GraphAPI(access_token)
+          profile = graph.get_object(data[self.username][1])
+          graph.put_wall_post(message)
+                   
--- test_broadcastme.py
+++ test_broadcastme.py
@@ -1,10 +1,18 @@
 import channels
+from unittest import mock
 
 def test_facebook_post():
-    A = channels.FaceBook('labeebg2@gmail.com', 'password')
-    message = '2nd refactor test post from channels.py'
-    acknowledgement = A.facebook_post(message)
-    assert acknowledgement is True
+    original_facebook = channels.facebook
+    channels.facebook = mock.Mock()
+    mock_graph = mock.Mock()
+    channels.facebook.GraphAPI.return_value = mock_graph
     
+    A = channels.FaceBook('labeebg2@gmail.com', 'password','fb_data.json')
+    message = 'unit test, dependency injection test'
+    A.facebook_post(message)
+
+    channels.facebook.GraphAPI.assert_called_with("EAACEdEose0cBAJxTg1E99MyFEZAEF1uohn5y7rGBqriXlVXl9ss0Jvg4vPAB6oWQRgQPuoQTPD6rzDI7MEB1XMw2UEI8kHlrDaNgRCDOEhDPtND56Lqy8Bxai37QiMrjN4DphMzo4kl6HoGxZAAQaMrJEsZCRZC0OigzrXgiCgZDZD")
+    mock_graph.get_object.assert_called_with("10202702084847551")
+    mock_graph.put_wall_post.assert_called_with("unit test, dependency injection test")
     
 

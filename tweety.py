a950bd975600e80d3e27b9f3de190004022424a2
abu <abukmca@gmail.com>
2016-09-19 18:23:32 +0530

    Add password for security

--- tweety.py
+++ tweety.py
@@ -3,17 +3,21 @@ import tweepy
 import json
 
 
-def load_data(id):
+def load_data(id,pwd):
     with open("db.json") as f:
         data=json.load(f)
-        global acc
-        acc=data[id]
-        return (acc)
+        global id_details
+        id_details=data[id]
+        id_pwd=id_details["PASSWORD"]
+        if pwd==id_pwd:
+            return (id_details)
+        else:
+            return(False)
 
 
-def details(id):
-    data=load_data(id)
-    id_data=data["twitter"]
+def details(id,pwd):
+    data=load_data(id,pwd)
+    id_data=data["TWITTER"]
     consumer_key=id_data[0]
     consumer_secret=id_data[1]
     access_token=id_data[2]
@@ -21,13 +25,11 @@ def details(id):
     return(consumer_key,consumer_secret,access_token,access_secret)
 
 
-def posting (id,msg):
-    ckey,csec,atoken,asec=details(id)
+def posting (ckey,csec,atoken,asec):
     auth= OAuthHandler(ckey,csec)
-    print(auth)
     auth.set_access_token(atoken,asec)
     tweepy.API(auth).update_status(msg)
-    print("tweet successfully")
+    return True
 
 
     
@@ -35,28 +37,26 @@ def new_twitter(uid,ck,cs,at,asc):
     with open ("db.json") as f:
         data=json.load(f)
         uid_data=data[uid]
-        if "twitter" not in uid_data:
-            data[uid]["twitter"]=[ck,cs,at,asc]
+        if "TWITTER" not in uid_data:
+            data[uid]["TWITTER"]=[ck,cs,at,asc]
             f=open("db.json","w")
             json.dump(data,f)
             f.close()
         else:
             print("User id Already Exist")
-        return (data)
 
 
     
-def new_account(id):
+def new_account(id,pwd):
       with open ("db.json") as f:
         data=json.load(f)
         if id not in data:
-            data[id]={}
+            data[id]={"PASSWORD":pwd}
             f=open("db.json","w")
             json.dump(data,f)
             f.close()
         else:
             print("User id Already Exist")
-        return (data)
     
     
     

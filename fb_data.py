2c5ca57983acaa40a895a434a1d613072e84b6d0
abu <abukmca@gmail.com>
2016-09-20 15:01:48 +0530

    Erron 3 =

--- fb_data.py
+++ fb_data.py
@@ -1,29 +1,17 @@
-# Import json module
 import json
+from common import load_data
 
-# defining load_file function
-def load_file(file):
-    # the argument is a filename
-    with open(file) as f:
-        # the file is opened in f variable
-        data = json.load(f)
-        # the content of f is loaded in data using
-        # the json function "load"
-    return data # data is returned
+def new_fb(uid, pwd, user_id, access_token):
+    data = load_data("db.json")
+    passw=data[uid]["PASSWORD"]
+    if pwd==passw:
+        if "FACEBOOK" not in data[uid]:
+            data[uid]["FACEBOOK"] = [user_id, access_token]
+            f = open("db.json","w")
+            json.dump(data, f)
+            f.close()
+        else:
+            print("User have already have Facebook")
+    else:
+        print("WRONG PASSWORD")
         
-        
-def new_fb(username, passwd, user_id, access_token): # adds a new user detail
-    data = load_file("fb_data.json")
-    # call the above method
-    if username not in data:
-        # checks wether the given usr_id is already in
-        # the loaded data or not and if not,
-        data[username] = [passwd, user_id, access_token]
-        # adds it to the loaded data by creating a new dictionary element
-        f = open("fb_data.json","w")
-        json.dump(data, f)
-        # dumps the new data to f
-        # overwrites data in f
-        f.close()
-               
-    

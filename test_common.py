2b6d5d3451eae3e6978e323f2211e3a81114bdd7
abu <abukmca@gmail.com>
2016-09-20 12:15:00 +0530

    add new method

--- test_common.py
+++ test_common.py
@@ -1,11 +1,15 @@
-from common import load_data,new_account
+from common import load_data,new_account,load_user_data
 
 def test_load_data():
-    b=load_data("db.json","one","pwd1")
+    b=load_data("db.json")
     assert len(b)>0
 
 
 def test_new_account():
     new_account("three","pwd3")
-    data=load_data("db.json","three","pwd3")
-    assert data=={"PASSWORD":"pwd3"}
+    data=load_data("db.json")
+    assert data["three"]=={"PASSWORD":"pwd3"}
+
+def test_load_user_data():
+    ud=load_user_data("db.json","three","pwd3")
+    assert ud=={"PASSWORD": "pwd3"}

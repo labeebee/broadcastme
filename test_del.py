e1c43afc4b9efecf0eee7e4ae7b1d8f8f7cf1ab1
abu <abukmca@gmail.com>
2016-09-24 15:24:16 +0530

    remved file

--- test_del.py
+++
@@ -1,5 +0,0 @@
-from broadcast_me import del_channel
-def test_del_channel():
-    
-   data =del_channel('fb')
-   assert 'fb' not in data 
--- test_fb_data.py
+++
@@ -1,7 +0,0 @@
-from fb_data import new_fb
-from common import load_data,new_account
-def test_new_fb():
-    new_account("fb_user","fb")
-    new_fb("fb_user","fb","user_id","accesstoken")
-    a=load_data("db.json")
-    assert len(a["fb_user"])>0

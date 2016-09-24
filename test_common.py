ec77890f73327c4286c0233dec4b16481543a434
abu <abukmca@gmail.com>
2016-09-24 15:09:52 +0530

    Red add new sigin test

--- test_common.py
+++ test_common.py
@@ -1,4 +1,4 @@
-from common import load_data,new_account,load_user_data
+from common import load_data,signup,load_user_data
 
 def test_load_data():
     b=load_data("db.json")
@@ -13,3 +13,7 @@ def test_new_account():
 def test_load_user_data():
     ud=load_user_data("db.json","three","pwd3")
     assert ud=={"PASSWORD": "pwd3"}
+
+def test_signin():
+    assert signin("one","pwd1")==True
+    

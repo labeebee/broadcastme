1f63f4ac1b22666e26987033e12989a5b6e70d7d
abu <abukmca@gmail.com>
2016-09-25 12:46:14 +0530

    Test after avoid a method and indipendend tests

--- test_common.py
+++ test_common.py
@@ -1,29 +1,16 @@
-<<<<<<< HEAD
-from common import load_data,new_account,load_user_data
-=======
 from common import load_data,signup,signin
->>>>>>> common
-
 def test_load_data():
     b=load_data("db.json")
     assert len(b)>0
 
 
 def test_new_account():
-<<<<<<< HEAD
-    new_account("three","pwd3")
-    data=load_data("db.json")
-    assert data["three"]=={"PASSWORD":"pwd3"}
-
-def test_load_user_data():
-    ud=load_user_data("db.json","three","pwd3")
-    assert ud=={"PASSWORD": "pwd3"}
-=======
     signup("three","pwd3")
     data=load_data("db.json")
     assert data["three"]=={"PASSWORD":"pwd3"}
 
 def test_signin():
-    assert signin("one","pwd1")==True
+      signup("one","pwd1")
+      assert signin("one","pwd1")==True
     
->>>>>>> common
+

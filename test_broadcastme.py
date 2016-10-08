bc45b2ff0327cb2390b29aad7713160a683a8685
Noufal Ibrahim <noufal@nibrahim.net.in>
2016-10-08 11:09:28 +0530

    Removes bad test

--- test_broadcastme.py
+++ test_broadcastme.py
@@ -1,8 +1,8 @@
 import channels
 from unittest import mock
 from common import signup,load_data
-from fb_data import new_fb
-def test_facebook_post():
+# from fb_data import new_fb
+def Xtest_facebook_post():
 
     signup("labeebg2@gmail.com","password")
     new_fb("labeebg2@gmail.com","10202702084847551","EAACEdEose0cBAJxTg1E99MyFEZAEF1uohn5y7rGBqriXlVXl9ss0Jvg4vPAB6oWQRgQPuoQTPD6rzDI7MEB1XMw2UEI8kHlrDaNgRCDOEhDPtND56Lqy8Bxai37QiMrjN4DphMzo4kl6HoGxZAAQaMrJEsZCRZC0OigzrXgiCgZDZD")
